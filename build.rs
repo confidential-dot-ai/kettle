ae5a8efc1d0cb9758711c31a55f80989086dfd79
André Arko <andre@arko.net>
2026-05-05 10:12:00 -0700

    exclude build dir from build

--- build.rs
+++ build.rs
@@ -1,3 +1,12 @@
+use shadow_rs::CARGO_MANIFEST_DIR;
+use std::collections::BTreeSet;
+
 fn main() {
-    shadow_rs::ShadowBuilder::builder().build().unwrap();
+    let mut deny = BTreeSet::new();
+    deny.insert(CARGO_MANIFEST_DIR);
+
+    shadow_rs::ShadowBuilder::builder()
+        .deny_const(deny)
+        .build()
+        .unwrap();
 }

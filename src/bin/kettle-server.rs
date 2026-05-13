7c77592aff63a3469a3af0ff0406c9a26da35892
André Arko <andre@arko.net>
2026-05-13 00:47:36 +0000

    git repo building server

--- src/bin/kettle-server.rs
+++ src/bin/kettle-server.rs
@@ -1,6 +1,6 @@
 use axum::Router;
 use axum::extract;
-use axum::http::{HeaderMap, StatusCode};
+use axum::http::StatusCode;
 use axum::routing::{get, post};
 use flate2::Compression;
 use flate2::read::GzDecoder;
@@ -45,15 +45,22 @@ async fn health() -> &'static str {
 }
 
 #[derive(Deserialize, Debug)]
+#[serde(untagged)]
 enum BuildArgs {
-    Repo { repo_url: String, repo_ref: String },
-    Tarball { tarball_data: Vec<u8> },
+    Repo {
+        repo_url: String,
+        repo_ref: String,
+        nonce: String,
+    },
+    Tarball {
+        tarball_data: Vec<u8>,
+        nonce: String,
+    },
 }
 
 async fn build_handler(
     extract::State(state): extract::State<Arc<AppState>>,
-    headers: HeaderMap,
-    extract::Json(payload): extract::Json<BuildArgs>,
+    extract::Json(args): extract::Json<BuildArgs>,
 ) -> Result<(StatusCode, [(&'static str, &'static str); 1], Vec<u8>), (StatusCode, String)> {
     let mut busy = state.busy.lock().await;
     if *busy {
@@ -61,41 +68,47 @@ async fn build_handler(
     }
     *busy = true;
 
-    let result = do_build(&headers, &payload).await;
+    let result = do_build(&args).await;
 
     *busy = false;
     result
 }
 
 async fn do_build(
-    headers: &HeaderMap,
     args: &BuildArgs,
 ) -> Result<(StatusCode, [(&'static str, &'static str); 1], Vec<u8>), (StatusCode, String)> {
     let work_dir = create_work_dir()?;
 
-    let nonce = get_nonce(headers)?;
-
     let project_dir = match args {
-        BuildArgs::Repo { repo_url, repo_ref } => {
-            repo_setup(&repo_url, &repo_ref, &nonce, &work_dir).await?
+        BuildArgs::Repo {
+            repo_url,
+            repo_ref,
+            nonce,
+        } => {
+            validate_nonce(nonce)?;
+            repo_setup(&repo_url, &repo_ref, &work_dir).await?
         }
-        BuildArgs::Tarball { tarball_data } => {
-            tarball_setup(&tarball_data, &nonce, &work_dir).await?
+        BuildArgs::Tarball {
+            tarball_data,
+            nonce,
+        } => {
+            validate_nonce(nonce)?;
+            tarball_setup(&tarball_data, &work_dir).await?
         }
     };
 
     #[cfg(feature = "attest")]
-    commands::attest::attest(AttestArgs {
-        path: project_dir.clone(),
-        nonce: Some(hex::encode(nonce_bytes)),
-    })
-    .map_err(|e| (StatusCode::CONFLICT, format!("build failed: {e}")))?;
+    {
+        commands::attest::attest(AttestArgs {
+            path: project_dir.clone(),
+            nonce: Some(args.nonce),
+        })
+        .map_err(|e| (StatusCode::CONFLICT, format!("build failed: {e}")))?;
+    }
 
     #[cfg(not(feature = "attest"))]
     commands::build::build(&project_dir)
         .map_err(|e| (StatusCode::CONFLICT, format!("build failed: {e}")))?;
-
-    nonce;
 
     // Tar up kettle-build/ directory as response
     let kettle_build_dir = project_dir.join("kettle-build");
@@ -134,14 +147,23 @@ async fn do_build(
 async fn repo_setup(
     repo_url: &str,
     repo_ref: &str,
-    nonce: &Vec<u8>,
     work_dir: &PathBuf,
 ) -> Result<PathBuf, (StatusCode, String)> {
-    std::process::Command::new("git")
-        .args(["clone", "--revision", repo_ref, "--shallow", "--", repo_url])
+    let output = std::process::Command::new("git")
+        .args([
+            "clone",
+            "--revision",
+            repo_ref,
+            "--depth",
+            "1",
+            "--",
+            repo_url,
+        ])
         .current_dir(&work_dir)
         .output()
         .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, format!("git clone: {e}")))?;
+    println!("{}", String::from_utf8_lossy(&output.stdout));
+    println!("{}", String::from_utf8_lossy(&output.stderr));
 
     let project_dir = find_project_dir(&work_dir)
         .map_err(|e| (StatusCode::BAD_REQUEST, format!("bad project layout: {e}")))?;
@@ -150,7 +172,6 @@ async fn repo_setup(
 
 async fn tarball_setup(
     tarball_data: &Vec<u8>,
-    nonce: &Vec<u8>,
     work_dir: &PathBuf,
 ) -> Result<PathBuf, (StatusCode, String)> {
     // Extract tarball
@@ -206,27 +227,6 @@ fn create_work_dir() -> Result<PathBuf, (StatusCode, String)> {
     Ok(work_dir)
 }
 
-fn get_nonce(headers: &HeaderMap) -> Result<Vec<u8>, (StatusCode, String)> {
-    let nonce_hex = headers
-        .get("x-nonce")
-        .and_then(|v| v.to_str().ok())
-        .ok_or_else(|| {
-            (
-                StatusCode::BAD_REQUEST,
-                "missing X-Nonce header".to_string(),
-            )
-        })?;
-    let nonce_bytes = hex::decode(nonce_hex)
-        .map_err(|e| (StatusCode::BAD_REQUEST, format!("invalid nonce hex: {e}")))?;
-    if nonce_bytes.len() > 32 {
-        return Err((
-            StatusCode::BAD_REQUEST,
-            "nonce must be at most 32 bytes".to_string(),
-        ));
-    }
-    Ok(nonce_bytes)
-}
-
 fn find_project_dir(work_dir: &PathBuf) -> Result<PathBuf, String> {
     let entries: Vec<_> = std::fs::read_dir(work_dir)
         .map_err(|e| e.to_string())?
@@ -238,4 +238,16 @@ fn find_project_dir(work_dir: &PathBuf) -> Result<PathBuf, String> {
     }
 
     Ok(work_dir.clone())
+}
+
+fn validate_nonce(nonce: &str) -> Result<(), (StatusCode, String)> {
+    let nonce_bytes = hex::decode(nonce)
+        .map_err(|e| (StatusCode::BAD_REQUEST, format!("invalid nonce hex: {e}")))?;
+    if nonce_bytes.len() > 32 {
+        return Err((
+            StatusCode::BAD_REQUEST,
+            "nonce must be at most 32 bytes".to_string(),
+        ));
+    };
+    Ok(())
 }
